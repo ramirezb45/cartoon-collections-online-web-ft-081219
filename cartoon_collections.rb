f6982536ce6f21dab61496ca03f73e3b3c71e10b
Brittney Ramirez <brittneyramirez79@gmail.com>
2019-09-01 17:35:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,12 +4,18 @@ puts "current_index: #{index} - #{item}"
 end
 
   
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet# code an argument here  
+  planeteer_calls = ["earth","wind","fire","water","heart"]
+  planeteer_calls.map {|string| string.upcase }
 end
 
 def long_planeteer_calls# code an argument here
-  # Your code here
+ def long_planeteer_calls (short_words,assorted_words)
+short_words = ["puff","go","two"]
+long_planeteer_calls(short_words)
+assorted_words = ["two","go","industrious","bop"]
+long_planeteer_calls(assorted_words)
+long_planeteer_calls.find?("short_words")
 end
 
 def find_the_cheese# code an argument here
