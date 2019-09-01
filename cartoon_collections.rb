cab90a2fa01c04dbb0badd2e4621caad2a241cb9
Brittney Ramirez <brittneyramirez79@gmail.com>
2019-09-01 17:57:44 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,16 +4,31 @@ puts "current_index: #{index} - #{item}"
 end
 
   
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet# code an argument here  
+  planeteer_calls = ["earth","wind","fire","water","heart"]
+  planeteer_calls.map {|string| string.upcase }
 end
 
 def long_planeteer_calls# code an argument here
-  # Your code here
+ def long_planeteer_calls (short_words,assorted_words)
+short_words = ["puff","go","two"]
+long_planeteer_calls(short_words)
+assorted_words = ["two","go","industrious","bop"]
+long_planeteer_calls(assorted_words)
+long_planeteer_calls.find?("short_words")
 end
 
 def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(cheddar,gouda,camembert)
+def no_cheese(bun,ham,chicken,tomato_soup)
+snacks = ["crackers","gouda","thyme"]
+  find_the_cheese(snacks)
+foods = ["bun","tomato_soup","chicken"]
+  find_the_cheese(foods) 
+soup = ["tomato soup","cheddar","oyster crackers","gouda"]
+  find_the_cheese(soup)
+end
+snacks.include?(find_the_cheese)
+end
 end
    
