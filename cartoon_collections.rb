916bba13cb4348a0fc6477e3012a53db62c0ce67
Brittney Ramirez <brittneyramirez79@gmail.com>
2019-09-01 17:56:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,6 @@ long_planeteer_calls.find?("short_words")
 end
 
 def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+
 end
    
